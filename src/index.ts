d7dcf1e17baa281422376d43e07a7cbf21eaa672
Allan <allan.mendes0976@gmail.com>
2024-10-30 10:26:37 -0300

    initial

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+// async function getCharactersApi() {
+//   const url: string = 'https://potterapi-fedeperin.vercel.app/pt/characters';
+
+//   try {
+//     const response = await fetch(url);
+//     if (!response.ok) {
+//       throw new Error(`Response status: ${response.status}`);
+//     }
+//     const json = await response.json();
+//     console.log(json);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// getCharactersApi();
+
+console.log('oi');
