5937abf3cb2a171bb7660157cd9ec956eff146c8
Allan <allan.mendes0976@gmail.com>
2024-10-30 22:34:33 -0300

    feat: connect to external api

---
+++ dist/index.js
@@ -0,0 +1,37 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const showItems = document.querySelector('.show-items');
+function getCharactersApi() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const url = 'https://potterapi-fedeperin.vercel.app/pt/characters';
+        const response = yield fetch(url);
+        try {
+            if (response.ok) {
+                const characters = yield response.json();
+                characters.forEach((element) => {
+                    const name = document.createElement('p');
+                    const img = document.createElement('img');
+                    if (element.fullName !== null && element.image !== null) {
+                        name.innerHTML = element.fullName;
+                        img.src = element.image;
+                    }
+                    showItems.appendChild(img);
+                    showItems.appendChild(name);
+                });
+            }
+        }
+        catch (error) {
+            console.error(error);
+            throw new Error(`Response status: ${response.status}`);
+        }
+    });
+}
+getCharactersApi();
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
-// async function getCharactersApi() {
-//   const url: string = 'https://potterapi-fedeperin.vercel.app/pt/characters';
+const showItems = document.querySelector('.show-items') as HTMLElement;
+async function getCharactersApi() {
+  const url: string = 'https://potterapi-fedeperin.vercel.app/pt/characters';
+  const response = await fetch(url);
+  try {
+    if (response.ok) {
+      const characters = await response.json();
+      characters.forEach((element: Record<string, null>) => {
+        const name = document.createElement('p');
+        const img = document.createElement('img');
+        if (element.fullName !== null && element.image !== null) {
+          name.innerHTML = element.fullName;
+          img.src = element.image;
+        }
+        showItems.appendChild(img);
+        showItems.appendChild(name);
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    throw new Error(`Response status: ${response.status}`);
+  }
+}
 
-//   try {
-//     const response = await fetch(url);
-//     if (!response.ok) {
-//       throw new Error(`Response status: ${response.status}`);
-//     }
-//     const json = await response.json();
-//     console.log(json);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-// getCharactersApi();
-
-console.log('oi');
+getCharactersApi();
